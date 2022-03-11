0085af4535b082bff88944f823208c424719dc9c
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2022-03-11 10:06:22 -0500

    Rework EC2 alarms to use metric_query
    This doesn't change anything, but this syntax makes more sense to me.

--- aws_ec2_alarms.tf
+++ aws_ec2_alarms.tf
@@ -31,22 +31,27 @@ resource "aws_cloudwatch_metric_alarm" "system_status_check" {
 resource "aws_cloudwatch_metric_alarm" "instance_status_check" {
   for_each = toset(var.instance_ids)
 
-  alarm_actions       = var.alarm_actions
-  alarm_description   = "Monitor EC2 instance status check"
-  alarm_name          = "ec2_instance_status_check_${each.value}"
-  comparison_operator = "GreaterThanThreshold"
-  dimensions = {
-    InstanceId = each.value
-  }
+  alarm_actions             = var.alarm_actions
+  alarm_description         = "Monitor EC2 instance status check"
+  alarm_name                = "ec2_instance_status_check_${each.value}"
+  comparison_operator       = "GreaterThanThreshold"
   evaluation_periods        = 1
   insufficient_data_actions = var.insufficient_data_actions
-  metric_name               = "StatusCheckFailed_Instance"
-  namespace                 = "AWS/EC2"
-  ok_actions                = var.ok_actions
-  # This is the minimum value for alarms in the AWS namespace.
-  period    = 60
-  statistic = "Maximum"
-  threshold = 0
+  metric_query {
+    id = "instance_status_check"
+    metric {
+      dimensions = {
+        InstanceId = each.value
+      }
+      metric_name = "StatusCheckFailed_Instance"
+      namespace   = "AWS/EC2"
+      period      = 60
+      stat        = "Maximum"
+    }
+    return_data = true
+  }
+  ok_actions = var.ok_actions
+  threshold  = 0
 }
 
 # This alarm should never go off if all instances are correctly
@@ -54,42 +59,53 @@ resource "aws_cloudwatch_metric_alarm" "instance_status_check" {
 resource "aws_cloudwatch_metric_alarm" "imdsv1_request" {
   for_each = toset(var.instance_ids)
 
-  alarm_actions       = var.alarm_actions
-  alarm_description   = "Monitor EC2 instance MetadataNoToken metric"
-  alarm_name          = "ec2_metadata_no_token_${each.value}"
-  comparison_operator = "GreaterThanThreshold"
-  dimensions = {
-    InstanceId = each.value
-  }
+  alarm_actions             = var.alarm_actions
+  alarm_description         = "Monitor EC2 instance MetadataNoToken metric"
+  alarm_name                = "ec2_metadata_no_token_${each.value}"
+  comparison_operator       = "GreaterThanThreshold"
   evaluation_periods        = 1
   insufficient_data_actions = var.insufficient_data_actions
-  metric_name               = "MetadataNoToken"
-  namespace                 = "AWS/EC2"
-  ok_actions                = var.ok_actions
-  # This metric does not need to be tracked minute to minute.
-  period    = 300
-  statistic = "Maximum"
-  threshold = 0
+  metric_query {
+    id = "imdsv1_request"
+    metric {
+      dimensions = {
+        InstanceId = each.value
+      }
+      metric_name = "MetadataNoToken"
+      namespace   = "AWS/EC2"
+      period      = 300
+      stat        = "Maximum"
+    }
+    return_data = true
+  }
+  ok_actions = var.ok_actions
+  threshold  = 0
 }
 
 # Alarm if we have 30 straight minutes of over 90% CPU utilization.
 resource "aws_cloudwatch_metric_alarm" "cpu_utilization" {
   for_each = toset(var.instance_ids)
 
-  alarm_actions       = var.alarm_actions
-  alarm_description   = "Monitor EC2 instance CPU utilization"
-  alarm_name          = "ec2_cpu_utilization_${each.value}"
-  comparison_operator = "GreaterThanThreshold"
-  datapoints_to_alarm = 6
-  dimensions = {
-    InstanceId = each.value
-  }
+  alarm_actions             = var.alarm_actions
+  alarm_description         = "Monitor EC2 instance CPU utilization"
+  alarm_name                = "ec2_cpu_utilization_${each.value}"
+  comparison_operator       = "GreaterThanThreshold"
+  datapoints_to_alarm       = 6
   evaluation_periods        = 6
   insufficient_data_actions = var.insufficient_data_actions
-  metric_name               = "CPUUtilization"
-  namespace                 = "AWS/EC2"
-  ok_actions                = var.ok_actions
-  period                    = 300
-  statistic                 = "Maximum"
-  threshold                 = 90.0
+  metric_query {
+    id = "cpu_utilization"
+    metric {
+      dimensions = {
+        InstanceId = each.value
+      }
+      metric_name = "CPUUtilization"
+      namespace   = "AWS/EC2"
+      period      = 300
+      stat        = "Maximum"
+    }
+    return_data = true
+  }
+  ok_actions = var.ok_actions
+  threshold  = 90.0
 }
